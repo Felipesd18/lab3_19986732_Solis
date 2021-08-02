025279f5f6cd89470ad8890c9102e41cbc02e19b
Felipe Solís <felipe.solis@usach.cl>
2021-08-02 12:54:44 -0400

    Se desarrollo y finalizo la clase Fecha.

--- src/main/java/lab3_Java/Fecha.java
+++ src/main/java/lab3_Java/Fecha.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package lab3_Java;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- *
+ * Clase para representar la Fecha
+ * Esta clase sera utilizada para las clases usuarios y publicaciones
+ * Se utilizara para obtener la fecha que se crean los usuarios y las publicaciones
  * @author Felipe Solís
  */
+
 public class Fecha {
     
+    private static String tiempoActual; //Variable que guardara en dato String la fecha
+    
+    //Metodo para obtener la fecha actual 
+    public static String obtenerFechaActual(){
+        Fecha.actualizarFechaActual(); //Actualizamos la fecha actual con el metodo actualizarFechaActual
+        return tiempoActual; //Retornamos el dato String con la representacion de la fecha
+    }
+    //Metodo para actualizar la fecha actual, ademas de pasar el dato a string 
+    public static void actualizarFechaActual(){
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); //Creamos un formato para la fecha
+        Date date = new Date(); //Obtenemos la fecha actual
+        Fecha.tiempoActual = formatoFecha.format(date); //Pasamos al formato creado la fecha actual
+    }
 }
