cb3aacef405d8242e6ff7c3999b65bbd5233e6c7
Felipe Solís <felipe.solis@usach.cl>
2021-08-02 17:53:41 -0400

    Se desarrollo un metodo para vertificar si existe un usuario en la clase ListaDeUsuarios.

--- src/main/java/lab3_Java/ListaDeUsuarios.java
+++ src/main/java/lab3_Java/ListaDeUsuarios.java
@@ -15,7 +15,24 @@ public class ListaDeUsuarios {
     
     ArrayList<Usuario> listaDeUsuarios = new ArrayList();
     
-    public void addUsuario(Usuario usuario){
+    public void addUsuario(Usuario usuario){ //Metodo que se encarga de agregar un usuario a la lista de usuarios y setear su ID a la correspondiente
+        usuario.setId(listaDeUsuarios.size() + 1);
         listaDeUsuarios.add(usuario);
+    }
+    
+    public Boolean existeUsuario(String nombre, String contrasenia){ //Metodo que se encarga de preguntar si existe un usuario con el mismo nombre o contrasenia en la lista
+        
+        String nombreUsuario; //Variable que guardara el nombre del usuario temporalmente de la lista
+        String contraseniaUsuario; //Variable que guardara la contrasenia del usuario temporalmente de la lista
+        
+        for(int i=0; i < listaDeUsuarios.size() ; i++){ //Recorremos la lista con el for  
+            nombreUsuario = listaDeUsuarios.get(i).getNombre(); //Guardamos el nombre del usuario de la posicion i
+            contraseniaUsuario = listaDeUsuarios.get(i).getContrasenia(); //Guardamos la contrasenia del usuario en la posicion i
+            
+            if((nombreUsuario.equals(nombre)) || (contraseniaUsuario.equals(contrasenia))){ //Preguntamos si el nombre o la contrasenia recien obtenidas con iguales a las que son proporsionadas
+                return true;
+            }
+        }
+        return false;
     }
 }
