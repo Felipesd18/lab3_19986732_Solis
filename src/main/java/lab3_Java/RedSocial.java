9830931643266b4ba1976c4fb3464b39c5e1ae6f
Felipe Solís <felipe.solis@usach.cl>
2021-08-03 12:38:23 -0400

    Se creo la interfaz RedSocial que sera la que guarde los metodos que realiza una red social.

---
+++ src/main/java/lab3_Java/RedSocial.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_Java;
+
+/**
+ * Interfaz que simulara el comportamiento de una RedSocial
+ * Esta interfaz esta encargada de crear metodos que utiliza una red social
+ * @author Felipe
+ */
+public interface RedSocial {
+    
+    
+    
+}
